7d74dab825051bb3edfb194b5ea5ab6637a3065b
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 18:01:08 +0400

    sum nums 0-100

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-//условные операторы   task 5
-let rating = prompt('Please input a student rating: ', ' ');
-if(rating<0){
-    console.log("Invalid input !");
-}else if(rating>=0 && rating<=19){
-	console.log("Your grade is  F");
-}else if(rating>=20 && rating<=39){
-    console.log("Your grade is  E");
-}else if(rating>=40 && rating<=59){
-    console.log("Your grade is  D");
-}else if(rating>=60 && rating<=74){
-    console.log("Your grade is  C");
-}else if(rating>=75 && rating<=89){
-    console.log("Your grade is  B");
-}else{
-    console.log("Your grade is A")
-} 
+// циклы task 1
+let nums=0;
+let sum=0;
+while(nums<=100){
+	 nums++;
+ if(nums%2==0){
+  sum+=nums;
+ }
+}console.log(sum);
