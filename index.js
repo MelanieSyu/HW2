752e272c79cf8ef4a7b1ddaf859ccd1fc3de6e6f
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 17:34:44 +0400

    conditional statements 2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-    //условные операторы   task 2
 let x= prompt('Input a coordination x:',' ');
 let y= prompt('Input a coordination y:',' ');
- 	let _x= (x>0)? [1,2] : [3,4];
-	let _y= (y>0)? [1,4] : [2,3];
-	console.log(`x: ${_x}`+ `y: ${_y}`);
+if(x>o && y>0){
+    console.log("first quarter");
+}else if(x>o && y<0){
+    console.log("fourth quarter");
+}else if(x<0 && y>0){
+    console.log("second quarter");
+}else if (x<0 && y<0){
+    console.log("third quarter");
+}else{
+    console.log("It's a centre");
+}
