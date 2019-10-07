b89845dfee7aa13278bd26a0240eb0a5e5a11c2a
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 17:27:16 +0400

    conditional statements task 1

---
+++ index.js
@@ -0,0 +1,8 @@
+//условные операторы   task 1
+let a= prompt('Input a first number :', ' ');
+let b= prompt('Input a second number :', '');
+if(a%2==0){
+    console.log(a*b); 
+}else{
+    console.log(a+b);
+}
