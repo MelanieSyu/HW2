a849a0fdddebae1d19cb1edc0db71a410a948c13
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 17:37:06 +0400

    conditional statements 3

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let x= prompt('Input a coordination x:',' ');
-let y= prompt('Input a coordination y:',' ');
-if(x>o && y>0){
-    console.log("first quarter");
-}else if(x>o && y<0){
-    console.log("fourth quarter");
-}else if(x<0 && y>0){
-    console.log("second quarter");
-}else if (x<0 && y<0){
-    console.log("third quarter");
-}else{
-    console.log("It's a centre");
+let number1 = prompt('Input a first num', '');
+let number2 = prompt('Input a second num', '');
+let number3 = prompt('Input a third num', '');
+
+let sum;
+if (number1>0){
+sum=number1;
 }
+if (number2>0){
+sum+=number2;
+}
+if (number3>0){
+sum+=number3;
+}
+	console.log(sum); 
