60185062c77cae7c9fca0e755077f34a4432209f
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 18:03:13 +0400

    simple or compound

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// циклы task 1
-let nums=0;
-let sum=0;
-while(nums<=100){
-	 nums++;
- if(nums%2==0){
-  sum+=nums;
- }
-}console.log(sum);
+// циклы task 2
+let a= prompt('Input a number: ',' ');
+if(a%2==0){	
+    console.log(`The number ${a} is compound`);	    
+}else{
+    console.log(`The number ${a} is simple`);
+} 	
