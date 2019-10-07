7cde3f0d8425eebfce58eb857e98824926d701a5
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 21:57:43 +0400

    sum positive numbers

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // массив  task 4
-let numbers=[12,-23,-7,4,9,0,24,-1];
-
-let max = numbers[0];
-let maxIndex = 0;
-
-
-for (var i = 1; i < numbers.length; i++) {
-  if (numbers[i] > max) {
-    maxIndex = i;
-    max = numbers[i];
-  }
+let positive = [];
+let summ = 0; 
+let numbers = [4, -1, 2, 5, -8, -5, 9, -4]; 
+for (let i = 0; i < numbers.length; i++) { 
+    if (numbers[i] > 0) {
+        positive.push(numbers[i]);
+    }    
 }
-console.log(maxIndex);
+for (let i = 0; i < positive.length; i++){
+    summ += positive[i];
+}
+console.log(summ); 
+
 
