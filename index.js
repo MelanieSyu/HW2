4a9bf6558828859885ad3e6aa48f9499cbf34390
MelanieSyu <mmilana.edu@gmail.com>
2019-10-07 21:04:16 +0400

    find min num

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// циклы task 6
-let number= 12345;
-console.log(`Before ${number}`);
-let mirror=0;
-while(number!=0){
-    mirror=mirror*10 + number%10;
-    number/=10;
-}console.log(`After ${mirror}`);
+// массив  task 1
+let arr=[12,3,-4,-13,7,9];
+let counter = arr.length;
+let min = arr[0];
+for (let i = 0; i < counter; i++) {
+  if (min > arr[i]) {
+    min = arr[i];
+  }
+}
+console.log(min);
