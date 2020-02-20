6a9467d54d39a2aae910962e4e9e96911d01f27c
MelanieSyu <mmilana.edu@gmail.com>
2020-02-20 20:59:12 +0400

    hm2 has been changed

--- index.js
+++ index.js
@@ -1,107 +1,226 @@
-//let a=2;
-//let b=3;
-//let result;
-
-const getProductOrSum= (first,second)=> first %2 ? first*second : first+second;
-//if(!(a%2)) {
- //   result= a+b;
-//} else {
- //   result=a*b;
-//}
-const getPositiveOfSum=(first, second, third) => {
-    let sum=0;
-
-    if(first>0){
-        sum+=first;
+//statements 1
+function count(first, second) {
+    let result;
+    if ((first % 2) === 0) {
+        result = first * second;
+    } else {
+        result = first + second;
     }
-    if(second>0){
-        sum+=second;
-    }
-    if(third>0){
-        sum+=third;
-    }
-    return sum;
 }
+console.log(count(4, 5));
 
-const getMax=(first, second, third) => { //
-   // correct
-} 
-
-const getMark= rate =>{
-    let result='';
-    if(rate<0 && rate>100){
-        return result;
-    } else if(rate>0 && rate<20){
-        result='F';
-    } else if(rate>=20 && rate<40){
-        result='E';
+//2
+function getQuarter(x, y) {
+    if (x>0 && y>0) {
+        console.log("1");
+    } else if (x<0 && y>0) {
+        console.log("2");
+    } else if (x<0 && y<0) {
+        console.log("3");
+    } else if (x>0 && y<0) {
+        console.log("4");  
+    } else if(x===0 && y>0){
+        console.log("1 and 2");
+    }else if(x===0 && y<0){
+        console.log("3 and 4");
+    }else{
+        console.log("x=0 and y=0");
     }
-    return result;
 }
+console.log(getQuarter(4, 5));
 
-const getEvenSum= () =>{ // =>2500;
-    let sum=0;
-    for (let i=1; i<100; i++){
-        if(i%2){
-            sum+=i;
+//3
+function getSumOfPositive(a, b, c) {
+    let arr = [];
+
+    let result = arr.reduce((sum, num) => (num >= 0)? sum + num : sum);
+    console.log(result);
+}
+console.log(getSumOfPositive(4, -5, 7));
+
+
+// 4
+function getSumOfMax(a, b, c) {
+    let sum = a+b+c;
+    let mult = a*b*c;
+    
+    let result = (sum > mult)? sum + 3 : mult + 3;
+    console.log(result);
+}
+console.log(getSumOfMax(4, -5, 6));
+
+
+// 5
+function getMark(mark) {
+    if (mark >= 0 && mark <= 19) {
+        console.log("f");
+    } else if (mark >= 20 && mark <= 39) {
+        console.log("e");
+    } else if (mark >= 40 && mark <= 59) {
+        console.log("d");
+    } else if (mark >= 60 && mark <= 74) {
+         console.log("c");
+    } else if (mark >= 75 && mark <= 89) {
+         console.log("b");
+    } else if (mark >= 90 && mark <= 100) {
+        console.log("a");
+    }
+}
+console.log(getMark(78));
+
+//cycle 1
+function getSum() {
+    let sum = 0;
+    for (let i = 0; i < 100; i++) {
+        if (i%2 === 0) {
+           sum += i;  
         }
-    }return sum;
-}
-getEvenSum();
-
-const getIsPrime= number => {
-    let tempNumber=number;
-    if(tempNumber<0){
-        tempNumber*=-1; // return false;
     }
-    for(let i=2; i<tempNumber/2; i++){
-        if(number%i){
-            return false;
+}
+
+// 2
+function getSimpleNumber(num) {
+    if ((num % 2) !== 0){
+        console.log('simple');
+    } else {
+        console.log('composite');
+    }
+}
+console.log(getSimpleNumber(23));
+
+// 4
+function getFactorial(num) {
+    let factorial = 1;
+
+    for (let i = 1; i <= num; i++) {
+        factorial *= i;
+    }
+    console.log(factorial);
+}
+console.log(getFactorial(5));
+
+// 5
+function getSumOfNum(num) {
+    let sum = 0;
+
+    while (a !== 0) {
+        sum += a % 10;
+        a = Math.floor(a / 10);
+    } 
+    console.log(sum);
+}
+console.log(getSumOfNum(12345));
+
+// 6
+function getReverse(num) {
+    let reverse = 0;
+
+    while (num) {
+        reverse *= 10;
+        reverse += num % 10;
+        num = Math.floor(num / 10);
+    }
+    console.log(reverse);
+}
+console.log(getReverse(12345));
+
+//array 1
+function getMax(array) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
-    }return true;
+    }
+    console.log(max);
 }
-const getSquareRoot= number =>{
-   let result=1;
-   for(let i=1; i<number/2, i**2<=number; i++){
-       result=i;
-       }
-   return result;
-} //getSquareRoot(16) // 4 
+console.log(getMax([-7, 23, -17, 0]));
 
-const getFactorial = number =>{
-    let result=1;
-    for(let i=number; i>0; i++){
-        result*=i;
-    }return result;
+//////////////////////////////////  3
+function getIndexMax(array) {
+    let max = arr[0];
+    let indexMax = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            indexMax = i;
+            max = arr[i];
+        }
+    } 
+    console.log(indexMax);
 }
+console.log(getIndexMax([-7, 23, -17, 0]));
 
-const getDigitSum= number =>{
-    let tempNumber= number;
-    let reminder=0;
-    let sum=0;
-    while(tempNumber>0){
-        reminder= tempNumber %10;
-        sum+= reminder;
-        tempNumber=(tempNumber-reminder /10;)
-    } return sum;
-}
+//////////////////////////////  5
+function getSumOfIndex(array) {
+    let sum  = 0;
 
-const getMirrorNumber= number =>{
-    let result=1;
-    let tempNumber=number;
-    let reminder=0;
-    while(tempNumber>0){
-        reminder=tempNumber % 10;
-        result+=reminder;
-        tempNumber=(tempNumber-reminder)/10;
-        reminder  &&(result*=10);
-    } return result;
+    for (let i = 1; i < arr.length; i++) {
+        if (i%2 !== 0) {
+            sum += arr[i];
+        }
+    }
+    console.log(sum);
 }
-const getMinIndex= array =>{
-    let minIndex=0;
-    let min= array.[minIndex];
-    for(array[i]<array[min]){
-        min= array[];
-        number
+console.log(getSumOfIndex([-7, 23, -17, 0]));
+
+///////////////////////////////// 6
+function getReverseArr(array) {
+    let arr2 = [];
+    let a = arr.length - 1;
+
+    for (let i = a; i >= 0; i--) {
+        arr2 += arr[i];
+    }
+    console.log(arr2);
+}
+console.log(getReverseArr([1, 2, 3, 4]));
+
+//////////////////////////////////// 7
+function getSumOfOdds(array) {
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i%2 !==0) {
+            result+=1;
+        } 
+    }
+    console.log(result);
+}
+console.log(getSumOfOdds( [1, 2, 3, 4]));
+
+//////////////////////////////////// 8
+
+
+//function
+function getWeekDays(weekday) {
+    let array = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    let day = ' ';
+
+    if (weekday > 0 && weekday < 8) {
+        for (let i = 0; i < array.length; i++) {
+            if (i === weekday - 1) {
+                day = array[i];
+            }
+        }
+        console.log(day);
     }
 }
+console.log('Today Is', getWeekDays(7));
+
+//BubleSorting
+
+function getSortingArray(arr) {
+    for (let i = 0; i < arr.length ; i++) {
+        for(let j = 0 ; j < arr.length; j++){ 
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    console.log(arr);
+  }
+console.log(getSortingArray([1,9,2,3,7,6,4,5,5]));
